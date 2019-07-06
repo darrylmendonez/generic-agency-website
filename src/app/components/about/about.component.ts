8729123dc9b0a83384dd6ee4020c12860c686474
Darryl Mendonez <darryl.mendonez@nucleuscentral.com>
2019-07-06 18:00:17 -0400

    pulling in json placeholder data for userProfiles

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { PlaceholdersService } from '../../services/placeholders.service';
+import { UserProfiles } from '../../models/userProfiles';
 import { SectionLayoutsData, GlobalData } from '../../data/sectionLayout.data';
 
 @Component({
@@ -7,12 +10,17 @@ import { SectionLayoutsData, GlobalData } from '../../data/sectionLayout.data';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
+  userProfiles: UserProfiles[];
   aboutSectionLayout = SectionLayoutsData[1];
   globalData = GlobalData;
 
-  constructor() { }
+  constructor(private placeholdersService: PlaceholdersService, private http: HttpClient) { }
 
   ngOnInit() {
+    this.placeholdersService.getUsers().subscribe(users => {
+      this.userProfiles = users;
+      console.log('userProfiles = ', this.userProfiles);
+    })
   }
 
 }
--- src/app/services/placeholders.service.ts
+++ src/app/services/placeholders.service.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { PlaceholderItems } from '../models/placeholderItems';
+import { UserProfiles } from '../models/userProfiles';
 
 @Injectable({
   providedIn: 'root'
@@ -10,11 +11,16 @@ import { PlaceholderItems } from '../models/placeholderItems';
 export class PlaceholdersService {
   itemsBaseUrl = 'https://jsonplaceholder.typicode.com/posts';
   itemsUrlOptions = '?_limit=30';
+  userBaseUrl = 'https://jsonplaceholder.typicode.com/users';
+  userUrlOptions = '?_limit=3';
 
   constructor(private http: HttpClient) { }
 
   getItems(): Observable<PlaceholderItems[]> {
     return this.http.get<PlaceholderItems[]>(`${this.itemsBaseUrl}${this.itemsUrlOptions}`);
+  }
+  getUsers(): Observable<UserProfiles[]> {
+    return this.http.get<UserProfiles[]>(`${this.userBaseUrl}${this.userUrlOptions}`);
   }
 
 }
